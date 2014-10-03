67a3a7ea2daaf6154b38e6db65084fef15582d38
Dimitri Korsch <korschdima@yahoo.de>
2014-10-03 11:07:46 +0200

    added secret key generation

--- djangostrap.py
+++ djangostrap.py
@@ -23,6 +23,11 @@ def replace(filepath, old_string, new_string):
     f.flush()
     f.close()
 
+
+def genereate_new_secret_key():
+  import random
+  return ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+
 if __name__ == "__main__":
   args = sys.argv
   if len(args) != 2: print("\n\tUsage: ./djangostrap.py <name_of_your_project>"); exit()
@@ -35,5 +40,8 @@ if __name__ == "__main__":
   shutil.copytree(".", new_project_dir, ignore = shutil.ignore_patterns("*.pyc", ".git"))
   rename(join(new_project_dir, "djangostrap"), join(new_project_dir, new_project_name))
   replace_in_dir(new_project_dir, "djangostrap", new_project_name)
+  with open(join(new_project_dir, "secret_key.txt"), "w") as f:
+    f.write(genereate_new_secret_key())
+    f.close()
 
   print("\n\tNew project was created in " + new_project_dir)
